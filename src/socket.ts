7719859e61fa98c6b3a9cd6803793a9abf3b0a11
Abhay Thakur <156583270+AbhayT1912@users.noreply.github.com>
2026-02-12 00:17:03 +0530

    Update socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000";
+   import.meta.env.VITE_API_BASE_URL;
 
 const socket = io(SOCKET_URL, {
   auth: {
